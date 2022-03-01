152c6a48e9899917db2be55f64777a103ce7610c
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-03-01 21:49:37 +0200

    Add devise and devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  authenticated :user do
+    root to: 'categories#index', as: :authenticated_root
+  end
+
+  root to: 'splash#index'
+
+  devise_for :users
+
+  resources :purchases
+  resources :categories
 end
