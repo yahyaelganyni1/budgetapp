f91b3f9fd5a2c572969ea476996996239f9924f0
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-03-03 05:01:24 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'splash/index'
-  get 'purchases/new'
-  get 'purchases/_form'
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
   authenticated :user do
     root to: 'categories#index', as: :authenticated_root
   end
 
-  root to: 'splash#index'
+  root to: "splash#index"
 
   devise_for :users
+
 
   resources :purchases
   resources :categories
